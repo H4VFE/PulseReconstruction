0cb8980e86902bdb0bde0576e464ea2ff48b1571
Marko Lazarevic <mr.marko.r@gmail.com>
2016-06-16 20:16:36 +0200

    newest analysis code

--- GoodEventSelector.C
+++ GoodEventSelector.C
@@ -33,24 +33,24 @@ TH1F* transform2Dto1D (TH2F *h2) {
 }
 
 int main() {
-	Int_t nbins = 800;
-    Int_t count = 0;
+	Int_t nbins = 800, count = 0;
+    Double_t integral;
 	TFile *input = new TFile("FilterRMSComparison.root");
-    TFile *templatefile = new TFile("/home/marko/Desktop/H4Analysis/ntuples/Templates_APDs.root");
+    TFile *templatefile = new TFile("/home/marko/Desktop/H4Analysis/ntuples/Templates_APDs.old.root");
 	TTree *MyTree = (TTree*) input->Get("RMS");
-    TCanvas *can1 = new TCanvas("can1", "canvas", 1200,1200);
+    TCanvas *can1 = new TCanvas("can1", "canvas", 1200,900);
     TCanvas *can2 = new TCanvas("can2", "canvas", 1200,1200);
     TCanvas *can3 = new TCanvas("can3", "canvas", 1200,1200);
     can1->Divide(1,3);
     can2->Divide(1,2);
     can3->Divide(1,2);
     MyTree->SetEntryList(0);
-    TString listcut = "abs(unfilteredbslope)<6 && unfilteredampfit>400";
+    TString listcut = "abs(unfilteredbslope)<6 && unfilteredampfit>700";
     MyTree->Draw(">>myList", listcut, "entrylist");
     TEntryList *myList = (TEntryList*) gDirectory->Get("myList");
     MyTree->SetEntryList(myList);
     Int_t nevents = myList->GetN();
-	MyTree->Draw("unfilteredevent", "abs(unfilteredbslope)<6 && unfilteredampfit>400", "goff");
+	MyTree->Draw("unfilteredevent", "abs(unfilteredbslope)<6 && unfilteredampfit>700", "goff");
     Double_t *vTemp = MyTree->GetV1();
     Int_t *vEvent = new Int_t[nevents];
     for (int iEntry = 0; iEntry<nevents; iEntry++) {
@@ -58,11 +58,15 @@ int main() {
     }
 	TString plot, plot2, cut;
 	char name[50];
-    TH1F *histoave = new TH1F("histoave","Wave Pulse Average", nbins, -42, 118);
+    TH1F *histoave = new TH1F("histoave","Wave Pulse Average", nbins, -40, 120);
+    TH1F *histoavefft = new TH1F("histoavefft","Wave Pulse Average FFT", nbins, 0, 5);
+    TH1F *histoaveph = new TH1F("histoaveph","Wave Pulse Average Phase", nbins, 0, 800);
     TH1F *originaltemplate = (TH1F*) templatefile->Get("APD2_E50_G50_prof");
     originaltemplate->Rebin(16);
-    TH1F *templatehisto = new TH1F("templatehisto", "Template = Green, Average = Red", 16000*800/1024, -42, 118); //12500 bins
-    for (Int_t i=0;i<16000*800/1024;i++) {
+    TH1F *templatehisto = new TH1F("templatehisto", "Template = Green, Average = Red", nbins, -40, 120); //12500 bins
+    TH1F *difference = new TH1F("difference","Template vs. Average Difference", nbins, -40, 120);
+    TH1F *percentdifference = new TH1F("percentdifference","Template vs. Average Percent Difference", nbins, -40, 120);
+    for (Int_t i=0;i<nbins;i++) {
         templatehisto->SetBinContent(i+1, originaltemplate->GetBinContent(i+1));
     }
     templatehisto->SetLineColor(kGreen+3);
@@ -70,32 +74,40 @@ int main() {
     can2->cd(2);
     histoave->GetYaxis()->SetRangeUser(-.120,1.2);
     //templatehisto->Draw();
+    histoave->SetStats(0);
     histoave->Draw();
 	for (Int_t i=0;i<nevents;i++) {
     //for (Int_t i=0;i<10;i++) {
+        if (vEvent[i]==513) continue; //event for which electronics die out for a bit halfway through
 		TString histoname = "TempHisto_";
         histoname += i;
         TString histoname2 = "TempHisto2_";
         histoname2 += i;
-        TH2F* TempHisto = new TH2F (histoname, "Temp Histo", nbins, -42, 118, 1000, -120, 800); //nanoseconds
-        TH2F* TempHisto2 = new TH2F (histoname2, "Temp Histo", nbins, -42, 118, 1000, -120, 800); //nanoseconds
+        TH2F* TempHisto = new TH2F (histoname, "Temp Histo", nbins, -40, 120, 1000, -120, 800); //nanoseconds
+        TH2F* TempHisto2 = new TH2F (histoname2, "Temp Histo", nbins, -40, 120, 1000, -120, 800); //nanoseconds
         TString h1name = "h1001_";
         h1name += i;
         TString h1name2 = "h1002_";
+        h1name2 += i;
+        TString h1name2fft = "h1002fft_";
+        h1name2 += i;
+        TString h1name2ph = "h1002ph_";
         h1name2 += i;
         TString h1name3 = "h1003_";
         h1name3 += i;
         TString h1name4 = "h1004_";
         h1name4 += i;
-        TH1F *h1001 = new TH1F(h1name,"Red = Unfiltered, Blue = Filtered", nbins, -42, 118);
-        TH1F *h1002 = new TH1F(h1name2,"h1002", nbins, -42, 118);
-        TH1F *h1003 = new TH1F(h1name3,"Filtered WF - Unfiltered WF", nbins, -42, 118);
-        TH1F *h1004 = new TH1F(h1name4,"Percent Change in Filtered WF - Unfiltered WF", nbins, -42, 118);
+        TH1F *h1001 = new TH1F(h1name,"Red = Unfiltered, Blue = Filtered", nbins, -40, 120);
+        TH1F *h1002 = new TH1F(h1name2,"h1002", nbins, -40, 120);
+        TH1F *h1002fft = new TH1F(h1name2fft,"h1002fft", nbins, 0, 5);
+        TH1F *h1002ph = new TH1F(h1name2ph,"h1002ph", nbins, 0, 800);
+        TH1F *h1003 = new TH1F(h1name3,"Filtered WF - Unfiltered WF", nbins, -40, 120);
+        TH1F *h1004 = new TH1F(h1name4,"Percent Change in Filtered WF - Unfiltered WF", nbins, -40, 120);
         plot = "unfilteredwfval:(unfilteredwftime-unfilteredtimeref)>>";
         plot += histoname;
         plot2 = "filteredwfval:(filteredwftime-filteredtimeref)>>";
         plot2 += histoname2;
-        cut = "abs(unfilteredbslope)<6 && unfilteredampfit>400 && unfilteredevent==";
+        cut = "abs(unfilteredbslope)<6 && unfilteredampfit>700 && unfilteredevent==";
         cut += vEvent[i];
         MyTree->Draw(plot, cut, "goff");
         TempHisto = (TH2F*) gDirectory->Get(histoname);
@@ -103,28 +115,36 @@ int main() {
         MyTree->Draw(plot2, cut, "goff");
         TempHisto2 = (TH2F*) gDirectory->Get(histoname2);
         h1002 = transform2Dto1D(TempHisto2);
+        if (h1002->GetBinCenter(h1002->GetMaximumBin()) < 30) continue;
         sprintf(name, "Good Events/Event%d", vEvent[i]);
         strcat(name, ".png");
         h1001->SetLineColor(kRed);
         h1002->SetLineColor(kBlue);
         h1001->SetStats(0);
         h1002->SetStats(0);
-        can1->cd(1);
+        //can1->cd(1);
+        can1->cd();
+        h1001->GetXaxis()->SetTitle("Time (ns)");
+        h1001->GetYaxis()->SetTitle("Amplitude");
         h1001->Draw();
         h1002->Draw("same");
-        for (Int_t i=0;i<nbins;i++) {
-            h1003->SetBinContent(i+1, (h1002->GetBinContent(i+1))-(h1001->GetBinContent(i+1)));
-            if (h1001->GetBinContent(i+1) != 0) h1004->SetBinContent(i+1, ((h1002->GetBinContent(i+1))-(h1001->GetBinContent(i+1)))/(h1001->GetBinContent(i+1)));
-            else h1004->SetBinContent(i+1, 0);
-        }
-        can1->cd(2);
-        h1003->Draw();
-        can1->cd(3);
-        h1004->Draw();
-        h1004->GetYaxis()->SetRangeUser(-0.1,0.1);
-        gPad->SetGrid();
+        //for (Int_t i=0;i<nbins;i++) {
+        //    h1003->SetBinContent(i+1, (h1002->GetBinContent(i+1))-(h1001->GetBinContent(i+1)));
+        //    if (h1001->GetBinContent(i+1) != 0) h1004->SetBinContent(i+1, ((h1002->GetBinContent(i+1))-(h1001->GetBinContent(i+1)))/(h1001->GetBinContent(i+1)));
+        //    else h1004->SetBinContent(i+1, 0);
+        //}
+        //can1->cd(2);
+        //h1003->Draw();
+        //can1->cd(3);
+        //h1004->Draw();
+        //h1004->GetYaxis()->SetRangeUser(-0.1,0.1);
+        //gPad->SetGrid();
         can1->SaveAs(name);
-        histoave->Add(histoave, h1002);
+        h1002->FFT(h1002fft, "MAG");
+        h1002->FFT(h1002ph, "PH");
+        histoavefft->Add(histoavefft, h1002fft);
+        histoaveph->Add(histoaveph, h1002ph);
+        //histoave->Add(histoave, h1002);
         can2->cd(2);
         h1002->Scale(1./(h1002->GetMaximum()));
         h1002->Draw("same");
@@ -133,17 +153,66 @@ int main() {
         gDirectory->Clear();
 	}
     can2->cd(1);
-    histoave->Scale(1./count);
-    histoave->SetLineColor(kRed);
-    histoave->SetLineWidth(4);
+    histoavefft->Scale(1./count);
+    histoaveph->Scale(1./count);
+    //histoave->Scale(1./count);
+    
+    Double_t *re_full = new Double_t[nbins];
+    Double_t *im_full = new Double_t[nbins];
+    TH1 *Throwaway = 0;
+    //TH1F *invhistoave = new TH1F(invhistoave, "Average Pulse", nbins, -40, 120);
+    TVirtualFFT *invFFT = TVirtualFFT::FFT(1, &nbins, "C2R M K");
+     for (Int_t n=0; n<nbins; n++) {
+        (re_full)[n]=(histoavefft->GetBinContent(n+1)*cos(histoaveph->GetBinContent(n+1)));
+        (im_full)[n]=(histoavefft->GetBinContent(n+1)*sin(histoaveph->GetBinContent(n+1)));
+    }
+    invFFT->SetPointsComplex(re_full, im_full);
+    invFFT->Transform();
+    Throwaway = TH1::TransformHisto(invFFT, Throwaway, "Re");
+    for (Int_t p=0; p<nbins; p++) {
+        histoave->SetBinContent(p+1, Throwaway->GetBinContent(p+1)/nbins);
+    }
     histoave->Scale(1./(histoave->GetMaximum()));
+    //histoave->SetLineColor(kRed);
+    //histoave->SetLineWidth(4);
+    integral = templatehisto->Integral();
+    templatehisto->Scale(1./integral);
+    templatehisto->SetStats(0);
     templatehisto->Draw();
-    histoave->Draw("same");
+    //integral = histoave->Integral();
+    //histoave->Scale(1./integral);
+    histoave->DrawClone("same");
+    //histoave->Scale(integral);
     can2->cd(2);
-    //templatehisto->Draw("same");
-    histoave->Draw("same");
+    histoave->GetXaxis()->SetTitle("Time (ns)");
+    histoave->GetYaxis()->SetTitle("Normalized Amplitude");
+    histoave->DrawClone("same");
     can2->SaveAs("AmpSpread.png");
     can2->SaveAs("AmpSpreadRoot.root");
+    TFile *output = new TFile("Alignment.root", "recreate");
+    output->cd();
+    originaltemplate->Write();
+    histoave->Write();
+    templatehisto->Write();
+    output->Close();
     can3->cd(1);
-
+    histoave->Scale(1./integral);
+    for (int i=0;i<nbins;i++) {
+        difference->SetBinContent(i+1, histoave->GetBinContent(i+1) - templatehisto->GetBinContent(i+1));
+        if (templatehisto->GetBinContent(i+1) != 0) percentdifference->SetBinContent(i+1, (histoave->GetBinContent(i+1) - templatehisto->GetBinContent(i+1))/templatehisto->GetBinContent(i+1));
+        else percentdifference->SetBinContent(i+1, 0);
+    }
+    difference->GetXaxis()->SetTitle("Time (ns)");
+    difference->GetYaxis()->SetTitle("Average - Template");
+    difference->SetStats(0);
+    difference->Draw();
+    can3->cd(2);
+    percentdifference->GetXaxis()->SetTitle("Time (ns)");
+    percentdifference->GetYaxis()->SetTitle("Percent Difference Average - Template");
+    percentdifference->SetStats(0);
+    percentdifference->Draw();
+    percentdifference->GetYaxis()->SetRangeUser(-0.1,0.1);
+    gPad->SetGrid();
+    can3->SaveAs("Difference.png");
+    can3->SaveAs("Difference.root");
 }
---
+++ TemplateComparison.C
@@ -0,0 +1,287 @@
+#include "TFile.h"
+#include "TH1F.h"
+#include "TH2F.h"
+#include "TTree.h"
+#include "TGraph.h"
+#include "TCanvas.h"
+using namespace std;
+
+void TemplateComparison() {
+	TFile *input = new TFile("/media/marko/TOSHIBA EXT/CERN/TB Timing Res/All ntuples/4467/analysis_4467original.root"); //original
+	TFile *input2 = new TFile("/media/marko/TOSHIBA EXT/CERN/TB Timing Res/All ntuples/4467/Old Template/analysis_4467.root"); //old template
+	TFile *input3 = new TFile("/media/marko/TOSHIBA EXT/CERN/TB Timing Res/All ntuples/4467/New Template/analysis_4467.root"); //new template, lower limit 80 (default)
+    //TFile *input3 = new TFile("/media/marko/TOSHIBA EXT/CERN/TB Timing Res/All ntuples/4443/New Template/analysis_4443lowerlimit80.root"); //new template, lower limit 80 (default) -- naming convention, same file as above line
+    //TFile *input4 = new TFile("/media/marko/TOSHIBA EXT/CERN/TB Timing Res/All ntuples/4443/New Template/analysis_4443lowerlimit160.root"); //new template, lower limit 160
+	TCanvas *c1 = new TCanvas("c1", "canvas", 1600, 900);
+	TCanvas *c2 = new TCanvas("c2", "canvas", 1600, 900);
+	TCanvas *c3 = new TCanvas("c3", "canvas", 1600, 900);
+    //TCanvas *c4 = new TCanvas("c4", "canvas", 1600, 900);
+	TTree *h4unf = (TTree*) input->Get("h4");
+	TTree *h4old = (TTree*) input2->Get("h4");
+	TTree *h4new = (TTree*) input3->Get("h4");
+    //TTree *h4new2 = (TTree*) input4->Get("h4");
+	TH1F *test = new TH1F ("test", "test", 200, 36, 42);
+	TH1F *Unf = new TH1F ("Unf", "unfiltered", 600, -3, 3);
+	TH1F *Old = new TH1F ("Old", "old template", 600, -3, 3);
+	TH1F *New = new TH1F ("New", "new template", 600, -3, 3);
+	TH1F *Unf2 = new TH1F ("Unf2", "unfiltered", 200, 0, 10);
+	TH1F *Old2 = new TH1F ("Old2", "old template", 200, 0, 10);
+	TH1F *New2 = new TH1F ("New2", "new template", 200, 0, 10);
+	Float_t mean, stddev;
+	TString drawtimeres, cut;
+	Int_t nspill = 102;
+
+    cut = "abs(b_slope[APD2])<6 && fit_ampl[APD2]>700 && amp_max[MCP1]>100 && spill==";
+    cut += nspill;
+	h4unf->Draw("fit_time[APD2]-time[MCP1]>>test", cut);
+	mean = test->GetMean();
+	drawtimeres = Form("fit_time[APD2]-time[MCP1]-%f>>Unf", mean);
+	cout << "unf mean = " << mean << endl;
+	h4unf->Draw(drawtimeres, cut);
+	
+    cut = "abs(b_slope[APD2])<6 && fit_ampl[APD2]>717 && amp_max[MCP1]>100 && spill==";
+    cut += nspill;
+	h4old->Draw("fit_time[APD2]-time[MCP1]>>test", cut);
+	mean = test->GetMean();
+	drawtimeres = Form("fit_time[APD2]-time[MCP1]-%f>>Old", mean);
+	h4old->Draw(drawtimeres, cut);
+	
+	h4new->Draw("fit_time[APD2]-time[MCP1]>>test", "abs(b_slope[APD2])<6 && fit_ampl[APD2]>717 && amp_max[MCP1]>100");
+	mean = test->GetMean();
+	drawtimeres = Form("fit_time[APD2]-time[MCP1]-%f>>New", mean);
+	cout << "filt mean = " << mean << endl;
+	h4new->Draw(drawtimeres, "abs(b_slope[APD2])<6 && fit_ampl[APD2]>717 && amp_max[MCP1]>100");
+
+    //cut = "abs(b_slope[APD2])<6 && fit_ampl[APD2]>717 && amp_max[MCP1]>100 && spill==";
+    //cut += nspill;
+    //h4new2->Draw("fit_time[APD2]-time[MCP1]>>test", cut);
+    //mean = test->GetMean();
+    //drawtimeres = Form("fit_time[APD2]-time[MCP1]-%f>>New", mean);
+    //cout << "filt mean = " << mean << endl;
+    //h4new2->Draw(drawtimeres, cut);
+	
+	c1->cd();
+    Unf->Scale(1./Unf->Integral());
+    Old->Scale(1./Old->Integral());
+    New->Scale(1./New->Integral());
+	Unf->SetLineColor(kRed);
+	Old->SetLineColor(kOrange);
+	New->SetLineColor(kBlue);
+	Unf->Draw();
+	Old->Draw("same");
+	New->Draw("same");
+	mean = New->GetMean();
+	stddev = New->GetStdDev();
+	cout << "New Template Fit" << endl;
+	New->Fit("gaus", "", "", mean-stddev, mean+stddev);
+	mean = Old->GetMean();
+	stddev = Old->GetStdDev();
+	cout << "Old Template Fit" << endl;
+	Old->Fit("gaus", "", "", mean-stddev, mean+stddev);
+	mean = Unf->GetMean();
+	stddev = Unf->GetStdDev();
+	cout << "Unfiltered Fit" << endl;
+	Unf->Fit("gaus", "", "", mean-stddev, mean+stddev);
+
+	cout << "Mean = " << Unf->GetMean() << "   Unfiltered Std Dev = " << Unf->GetStdDev() << "+-" << Unf->GetStdDevError() << " & Fit Std Dev = " << Unf->GetFunction("gaus")->GetParameter(2) << "+-" << Unf->GetFunction("gaus")->GetParError(2) << endl;
+	cout << "Mean = " << Old->GetMean() << "  Old Template Std Dev = " << Old->GetStdDev() << "+-" << Old->GetStdDevError() << " & Fit Std Dev = " << Old->GetFunction("gaus")->GetParameter(2) << "+-" << Old->GetFunction("gaus")->GetParError(2) << endl;
+	cout << "Mean = " << New->GetMean() << " New Template Std Dev = " << New->GetStdDev() << "+-" << New->GetStdDevError() << " & Fit Std Dev = " << New->GetFunction("gaus")->GetParameter(2) << "+-" << New->GetFunction("gaus")->GetParError(2) << endl;
+	cout << "percent change = " << (New->GetStdDev()-Unf->GetStdDev())/Unf->GetStdDev()*100 << endl;
+	cout << "percent change fit = " << (New->GetFunction("gaus")->GetParameter(2)-Unf->GetFunction("gaus")->GetParameter(2))/Unf->GetFunction("gaus")->GetParameter(2)*100 << endl;
+
+
+
+	c2->cd();
+	h4unf->Draw("b_rms[APD2]>>Unf2", "abs(b_slope[APD2])<6 && fit_ampl[APD2]>700 && amp_max[MCP1]>100");
+	h4old->Draw("b_rms[APD2]>>Old2", "abs(b_slope[APD2])<6 && fit_ampl[APD2]>700 && amp_max[MCP1]>100");
+	h4new->Draw("b_rms[APD2]>>New2", "abs(b_slope[APD2])<6 && fit_ampl[APD2]>700 && amp_max[MCP1]>100");
+	Unf2->SetLineColor(kRed);
+	Old2->SetLineColor(kOrange);
+	New2->SetLineColor(kBlue);
+	Unf2->Draw();
+	Old2->Draw("same");
+	New2->Draw("same");
+	c3->cd();
+
+	h4unf->SetEntryList(0);
+    TString listcut = "fit_ampl[APD2]>700 && WF_ch==2 && spill==";
+    listcut += nspill;
+    h4unf->Draw(">>myList", listcut, "entrylist");
+    TEntryList *myList = (TEntryList*) gDirectory->Get("myList");
+    h4unf->SetEntryList(myList);
+    Int_t nevents = myList->GetN();
+
+	h4unf->SetEntryList(0);
+    listcut = "abs(b_slope[APD2])<6 && fit_ampl[APD2]>700 && amp_max[MCP1]>100 && WF_ch==2 && spill==";
+    listcut += nspill;
+    h4unf->Draw(">>myList", listcut, "entrylist");
+    myList = (TEntryList*) gDirectory->Get("myList");
+    h4unf->SetEntryList(myList);
+    Int_t neventsunf = myList->GetN();
+
+    h4new->SetEntryList(0);
+    listcut = "abs(b_slope[APD2])<6 && fit_ampl[APD2]>717 && amp_max[MCP1]>100 && WF_ch==2 && spill==";
+    listcut += nspill;
+    h4new->Draw(">>myList", listcut, "entrylist");
+    myList = (TEntryList*) gDirectory->Get("myList");
+    h4new->SetEntryList(myList);
+    Int_t neventsnew = myList->GetN();
+
+    //h4new2->SetEntryList(0);
+    //listcut = "abs(b_slope[APD2])<6 && fit_ampl[APD2]>717 && amp_max[MCP1]>100 && WF_ch==2 && spill==";
+    //listcut += nspill;
+    //h4new2->Draw(">>myList", listcut, "entrylist");
+    //myList = (TEntryList*) gDirectory->Get("myList");
+    //h4new2->SetEntryList(myList);
+    //Int_t neventsnew2 = myList->GetN();
+
+    cut = "abs(b_slope[APD2])<6 && fit_ampl[APD2]>700 && amp_max[MCP1]>100 && spill==";
+    cut += nspill;
+    h4unf->Draw("b_rms[APD2]", cut, "goff");
+    Double_t *vTemp_uBrms = h4unf->GetV1();
+    Double_t *vuBrms = new Double_t[neventsunf];
+    for (int iEntry = 0; iEntry<neventsunf; iEntry++) {
+        vuBrms[iEntry] = vTemp_uBrms[iEntry];
+    }
+
+    cut = "abs(b_slope[APD2])<6 && fit_ampl[APD2]>717 && amp_max[MCP1]>100 && spill==";
+    cut += nspill;
+    h4new->Draw("b_rms[APD2]", cut, "goff");
+    Double_t *vTemp_fBrms = h4new->GetV1();
+    Double_t *vfBrms = new Double_t[neventsnew];
+    for (int iEntry = 0; iEntry<neventsnew; iEntry++) {
+        vfBrms[iEntry] = vTemp_fBrms[iEntry];
+    }
+
+    //cut = "abs(b_slope[APD2])<6 && fit_ampl[APD2]>717 && amp_max[MCP1]>100 && spill==";
+    //cut += nspill;
+    //h4new2->Draw("b_rms[APD2]", cut, "goff");
+    //Double_t *vTemp_fBrms2 = h4new2->GetV1();
+    //Double_t *vfBrms2 = new Double_t[neventsnew2];
+    //for (int iEntry = 0; iEntry<neventsnew2; iEntry++) {
+    //    vfBrms2[iEntry] = vTemp_fBrms2[iEntry];
+    //}
+
+    cut = "abs(b_slope[APD2])<6 && fit_ampl[APD2]>700 && amp_max[MCP1]>100 && spill==";
+    cut += nspill;
+	h4unf->Draw("fit_ampl[APD2]", cut, "goff");
+    Double_t *vTemp_uAmpfit = h4unf->GetV1();
+    Double_t *vuAmpfit = new Double_t[neventsunf];
+    for (int iEntry = 0; iEntry<neventsunf; iEntry++) {
+        vuAmpfit[iEntry] = vTemp_uAmpfit[iEntry];
+    }
+
+    cut = "abs(b_slope[APD2])<6 && fit_ampl[APD2]>717 && amp_max[MCP1]>100 && spill==";
+    cut += nspill;
+    h4new->Draw("fit_ampl[APD2]", cut, "goff");
+    Double_t *vTemp_fAmpfit = h4new->GetV1();
+    Double_t *vfAmpfit = new Double_t[neventsnew];
+    for (int iEntry = 0; iEntry<neventsnew; iEntry++) {
+        vfAmpfit[iEntry] = vTemp_fAmpfit[iEntry];
+    }
+
+    //cut = "abs(b_slope[APD2])<6 && fit_ampl[APD2]>717 && amp_max[MCP1]>100 && spill==";
+    //cut += nspill;
+    //h4new2->Draw("fit_ampl[APD2]", cut, "goff");
+    //Double_t *vTemp_fAmpfit2 = h4new->GetV1();
+    //Double_t *vfAmpfit2 = new Double_t[neventsnew2];
+    //for (int iEntry = 0; iEntry<neventsnew2; iEntry++) {
+    //    vfAmpfit2[iEntry] = vTemp_fAmpfit2[iEntry];
+    //}
+
+    cut = "spill ==";
+    cut += nspill;
+    h4unf->Draw("fit_time[APD2]", cut, "goff");
+    Double_t *vTemp_uTime = h4unf->GetV1();
+    Double_t *vuTime = new Double_t[neventsunf];
+    for (int iEntry = 0; iEntry<neventsunf; iEntry++) {
+        vuTime[iEntry] = vTemp_uTime[iEntry];
+    }
+
+    cut = "spill ==";
+    cut += nspill;
+    h4new->Draw("fit_time[APD2]", cut, "goff");
+    Double_t *vTemp_fTime = h4new->GetV1();
+    Double_t *vfTime = new Double_t[neventsunf];
+    for (int iEntry = 0; iEntry<neventsunf; iEntry++) {
+        vfTime[iEntry] = vTemp_fTime[iEntry];
+    }
+	c3->Divide(2,2);
+    //c3->cd(1);
+    //TGraph *graph = new TGraph(nevents, vuAmpfit, vfAmpfit);
+    //graph->Draw();
+    //graph->Fit("pol1", "", "", 400, 900);
+    //cout << "Filtered Cut Value = " << graph->GetFunction("pol1")->GetParameter(0) + graph->GetFunction("pol1")->GetParameter(1)*700 << endl;
+    //c3->cd(2);
+    //TGraph *graph2 = new TGraph(nevents, vuTime, vfTime);
+    //graph2->Draw();
+
+
+    c3->cd(3);
+    TH1F *maxovernoiseratio = new TH1F("maxovernoiseratio", "maxovernoiseratio", 50, -20, 800);
+    TH1F *maxovernoiseratio2 = new TH1F("maxovernoiseratio2", "maxovernoiseratio2", 50, -20, 800);
+    for (int iEntry = 0; iEntry<neventsunf; iEntry++) {
+    	maxovernoiseratio->Fill(vuAmpfit[iEntry]/vuBrms[iEntry]);
+    }
+    for (int iEntry = 0; iEntry<neventsnew; iEntry++) {
+    	maxovernoiseratio2->Fill(vfAmpfit[iEntry]/vfBrms[iEntry]);
+    }
+    maxovernoiseratio->SetLineColor(kRed);
+	maxovernoiseratio2->SetLineColor(kBlue);
+    maxovernoiseratio->DrawClone();
+    maxovernoiseratio2->DrawClone("same");
+
+    c3->cd(4);
+    TH1F *timeratio = new TH1F("timeratio", "timeratio", 1000, 0, 2);
+    for (int iEntry = 0; iEntry<nevents; iEntry++) {
+    	timeratio->Fill(vfTime[iEntry]/vuTime[iEntry]);
+    }
+    timeratio->Draw();
+
+    c3->cd(1);
+    TH1F *maxovernoiseratio160 = new TH1F("maxovernoiseratio160", "maxovernoiseratio160", 50, 0, 800);
+    //for (int iEntry = 0; iEntry<neventsnew; iEntry++) {
+    //    maxovernoiseratio160->Fill(vfAmpfit2[iEntry]/vfBrms2[iEntry]);
+    //}
+    maxovernoiseratio2->SetLineColor(kRed);
+    //maxovernoiseratio160->SetLineColor(kBlue);
+    maxovernoiseratio2->Draw();
+    //maxovernoiseratio160->Draw("same");
+
+    c3->cd(2);
+    TH1F *max = new TH1F("max", "max", 100, 0, 2000);
+    TH1F *max160 = new TH1F("max160", "max160", 100, 600, 1000);
+    h4new->Draw("fit_ampl[APD2]>>max");
+    //h4new2->Draw("fit_ampl[APD2]>>max160");
+    max->SetLineColor(kRed);
+    //max160->SetLineColor(kBlue);
+    //max160->Draw();
+    max->Draw("same");
+
+    TString filename = "spill";
+    filename += nspill;
+    filename += ".root";
+    TFile *outfile = new TFile(filename, "recreate");
+    outfile->cd();
+    Unf->Write();
+    Old->Write();
+    New->Write();
+    Unf2->Write();
+    Old2->Write();
+    New2->Write();
+
+
+	//h4unf->Draw("event", "spill==1", "goff");
+    //Double_t *vTemp = h4unfiltered->GetV1();
+    //Int_t *vEvent = new Int_t[nevents];
+    //for (int iEntry = 0; iEntry<nevents; iEntry++) {
+    //    vEvent[iEntry] = vTemp[iEntry];
+    //}
+    //h4unf->SetEntryList(0);
+    //TString listcut = "WF_ch==2 && spill==1";
+    //h4unfiltered->Draw(">>myList", listcut, "entrylist");
+    //TEntryList *myList = (TEntryList*) gDirectory->Get("myList");
+    //h4unfiltered->SetEntryList(myList);
+    //Int_t nevents = myList->GetN();
+	//for (int i=0; i<)
+}
